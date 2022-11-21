060b62a30d0ce2eb08e27ea8fca23cea01648695
FreakyFennec <ahjinani@gmail.com>
2022-11-21 07:20:33 +0100

    Added irregulars verbs to test site

---
+++ IrregularsVerbs/blog/assets/js/menu-hamburger-lateral-1-02.js
@@ -0,0 +1,22 @@
+/*
+  style-menu-hamgurger-lateral-1-02.css
+  =============================================
+  By AHJ
+*/
+
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
